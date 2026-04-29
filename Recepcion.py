f05e470a4ba0e7d7b77667bbfbab543dd66a3b3e
Jesus <karolrobledo05@gmail.com>
2026-04-29 09:53:47 -0600

    solo interfaz

--- Recepcion.py
+++ Recepcion.py
@@ -1,113 +1,138 @@
 
+#! Interfaz sin funcionamiento 
 
-#* el codigo no esta terminado
+from customtkinter import *
+import customtkinter as ctk
+from tkinter import messagebox
+from CTkTable import CTkTable
 
-import random
+# Configuración de estilo
+ctk.set_appearance_mode("dark") 
+ctk.set_default_color_theme("blue")
 
 class Recepcion:
     
-    def Datos(self):
-        self.n = int(input("Cuantas iteraciones quieres: "))
-        self.Recepcionistas = int(input("Cuantos recepcionistas quieres: "))
+    def __init__(self):
+        self.Interfaz = ctk.CTk()
         
-    def Tabla_1(self):
-        #tiempo entre llegadas
+        self.Interfaz.title("Interfaz --- Recepcion")
         
-        self.Tiempo_llegadas = []
+        # self.Interfaz.geometry("1500x800")
         
-        for _ in range(self.n):
+        # try: self.principal.state("zoomed")
+        # except: self.principal.attributes("-zoomed", True)
+        
+        
+        self.Interfaz.grid_columnconfigure(1, weight=1)
+        self.Interfaz.grid_rowconfigure(0, weight=1)
+        
+        #---------------------------------------------------------
+        
+        #PANEL IZQUIERDO (Solo para pedir los datos)
+        self.Frame_izq = CTkFrame(self.Interfaz, width=200, corner_radius=10)
+        self.Frame_izq.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        
+        
+        #Titulo (frame izq)
+        self.Titulo_frame_izq = CTkLabel(self.Frame_izq, text= "DATOS", font= ("Arial", 25 ))
+        self.Titulo_frame_izq.pack(pady=20, padx=10)
+        
+        #Cuadros de texto
+        
+        self.prueba_1 = CTkEntry(self.Frame_izq, placeholder_text="Dato 1", width= 130)
+        self.prueba_1.pack(pady=20, padx=10)
+        
+        self.prueba_2 = CTkEntry(self.Frame_izq, placeholder_text="Dato 2", width= 130)
+        self.prueba_2.pack(pady=20, padx=10)
+        
+        self.prueba_3 = CTkEntry(self.Frame_izq, placeholder_text="Dato 3", width= 130)
+        self.prueba_3.pack(pady=20, padx=10)
+        
+        self.prueba_4 = CTkEntry(self.Frame_izq, placeholder_text="Dato 4", width= 130)
+        self.prueba_4.pack(pady=20, padx=10)
+        
+        self.prueba_5 = CTkEntry(self.Frame_izq, placeholder_text="Dato 5", width= 130)
+        self.prueba_5.pack(pady=20, padx=10)
+        
+        self.prueba_6 = CTkEntry(self.Frame_izq, placeholder_text="Dato 6", width= 130)
+        self.prueba_6.pack(pady=20, padx=10)
+        
+        #Boton de simular
+        
+        self.btn_generar = CTkButton(self.Frame_izq, text= "simular", font= ("Arial", 25) , command= self.Creacion_tabla)
+        self.btn_generar.pack(pady=10)
+        
+        #---------------------------------------------------------
+        
+        #FRAME DERECHO (SOLO PARA MOSTRAR LOS DATOS)
+        
+        self.Frame_Der = CTkFrame(self.Interfaz, width=200, corner_radius=10)
+        self.Frame_Der.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+        
+        #Titulo (frame der)
+        
+        self.Titulo_frame_der = CTkLabel(self.Frame_Der, text= "MONTE CARLO", font= ("Arial", 30 ))
+        
+        self.Titulo_frame_der.pack(pady=10, padx=10)
+        
+        
+        #Tabla
+        
+        # Para que la tabla no se salga de la pantalla
+        self.scroll_frame = CTkScrollableFrame(self.Frame_Der)
+        self.scroll_frame.pack(expand=True, fill="both", padx=10, pady=10)
+        
+        #---------------------------------------------------------
+        
+        self.Tabla = None
+        
+        self.Interfaz.update() 
+        self.Interfaz.state("zoomed") #windows
+        # self.Interfaz.attributes('-zoomed', True) #Linux
+        
+        self.Interfaz.mainloop()
+        
+        #---------------------------------------------------------
+        
+    def Creacion_tabla(self):
+        
+        #obtener los datos para hacer la tabla
+        
+        # if not self.prueba_1.get() or not self.prueba_2.get() or not self.prueba_3.get() or not self.prueba_4.get():
+        #     messagebox.showwarning("CAMPOS","Complete los campos vacios", parent= self.Interfaz)
+        #     return False
+        
+        try:
             
-            num_ale = random.random()
+            # filas_predeterminadas = 10
+            filas_predeterminadas = int(self.prueba_1.get())
             
-            if num_ale > 0 and num_ale < 0.02:
-                self.Tiempo_llegadas.append(1)
-                
-            elif num_ale > 0.0201 and num_ale < 0.07:
-                self.Tiempo_llegadas.append(2)
-                
-            elif num_ale > 0.0701 and num_ale < 0.15:
-                self.Tiempo_llegadas.append(3)
-                
-            elif num_ale > 0.1501 and num_ale < 0.27:
-                self.Tiempo_llegadas.append(4)
-                
-            elif num_ale > 0.2701 and num_ale < 0.5:
-                self.Tiempo_llegadas.append(5)
-                
-            elif num_ale > 0.501 and num_ale < 0.73:
-                self.Tiempo_llegadas.append(6)
-                
-            elif num_ale > 0.7301 and num_ale < 0.85:
-                self.Tiempo_llegadas.append(7)
-                
-            elif num_ale > 0.8501 and num_ale < 0.93:
-                self.Tiempo_llegadas.append(8)
-                
-            elif num_ale > 0.9301 and num_ale < 0.98:
-                self.Tiempo_llegadas.append(9)
-                
-            elif num_ale > 0.9801 and num_ale < 1:
-                self.Tiempo_llegadas.append(10)
-                
-        print(F"Minutos: {self.Tiempo_llegadas}")
-    
-    def Hora(self):
-        
-        hora = 8
-        
-        minutos = 0
-        lim_minutos = 60
-        aux = 0
-        
-        for i in self.Tiempo_llegadas:
+        except Exception:
+                messagebox.showerror("INVALIDACION","Datos invalidos", parent= self.Interfaz)
+                return False
             
-            minutos += i
+        if self.Tabla:
+            self.Tabla.destroy()
+        
+        encabezados = [["Dias", "Aleatorio\npersonas", "Personas\npor dia", "Aleatorio\ngrupo", "Personas\npor grupo", "Grupos atendidos", "Aleatorio\nhabitacion", "Precio", "Aleatorio\nservicio", "Servicio", "Aleatorio\nrecepcio-\nnistas", "Recepcio-\nnistas", "Aleatorio\natencion", "Atencion", "Aleatorio\nespera\nen cola", "Espera\nen cola", "Aleatorio\nabandono", "Abandono", "Ingresos\nbrutos", "Costo diario", "Ganan-\ncia\nneta"]]
+        
+        for i in range(filas_predeterminadas):
+            fila_vacia = [str(i+1)] + [""] * 20
+            encabezados.append(fila_vacia)
             
-            #esto es provicional 
-            if minutos >= 60:
-                aux = minutos - lim_minutos
-                minutos = 0
-                hora += 1
-                minutos += aux
-                
-            print(F"Hora: {hora:02d}, Minutos: {minutos:02d}")
-            # print(F"{hora}:{minutos}")
+        #crear la tabla nueva
         
-    def Tabla_2(self):
-        #tiempo de atencion
+        self.Tabla = CTkTable(
+            master = self.scroll_frame, 
+            row = filas_predeterminadas + 1, 
+            column= 21, 
+            values= encabezados,
+            width = 100,
+            justify = "center",
+            wraplength = 100
+        )
         
-        self.Tiempo = []
+        #mostrar tabla
+        self.Tabla.pack(expand=True, fill="both", padx=10, pady=10)
         
-        tam = self.n * self.Recepcionistas
-        
-        for _ in range(tam):
-            #para poder hacer esto se podria hacer algo mas simple multiplicar n por la cantidad de recepcionistas y si llega a n ya se acabaron los numeros del primer recepcionista 
-
-            num_ale = random.random()
-            
-            if num_ale > 0 and num_ale < 0.49:
-                self.Tiempo.append(5)
-                
-            elif num_ale > 0.4901 and num_ale < 0.73:
-                self.Tiempo.append(10)
-                
-            elif num_ale > 0.7301 and num_ale < 0.88:
-                self.Tiempo.append(15)
-                
-            elif num_ale > 0.8801 and num_ale < 1:
-                self.Tiempo.append(20)
-                
-        for i in range(len(self.Tiempo)):
-            print(F"Minutos: {self.Tiempo[i]}")
-            
-            if i == self.n - 1:
-                print("\n")
-        
-    def Tiempo_inicial_final(self):
-        pass
-    
-R = Recepcion()
-R.Datos()
-R.Tabla_1()
-R.Hora()
-R.Tabla_2()
+Recepcion()
